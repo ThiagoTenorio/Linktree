61dc12994699a6edebcc4996423f89a959ed8055
THIAGO TENORIO <thiago_tenorio43@hotmail.com>
2023-08-01 22:58:02 -0300

    Projeto

--- script.js
+++ script.js
@@ -1,35 +1,25 @@
-// const form = document.getElementById('form');
+const form = document.getElementById('form');
 
-// form.addEventListener('submit', function (event) {
-//   event.preventDefault();
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
 
-//   const largura = document.getElementById('largura').value;
-//   const altura = document.getElementById('altura').value;
-//   const valor = document.getElementById('valor').value;
-
-//   const areaTotal = (largura * altura).toFixed(2);
-//   const valorTotal = (areaTotal * valor).toFixed(2);
-
-//   const metroQuadrado = document.getElementById('metro-quadrado');
-//   const valorFinal = document.getElementById('valor-final');
-
-//   if ()
-
-// })
-
-function calcularValorTotal() {
   const valor = parseFloat(document.getElementById('valor').value);
   const largura = parseFloat(document.getElementById('largura').value);
   const altura = parseFloat(document.getElementById('altura').value);
+  const vernizsim = document.getElementById('vernizsim').checked;
 
   const metroQuadrado = largura * altura;
-  const valorTotal = metroQuadrado * valor;
 
-  if (isNaN(valor) || isNaN(largura) || isNaN(altura) || valor <= 0 || largura <= 0 || altura <= 0) {
+  let valorTotal = metroQuadrado * valor;
+
+  if (valor <= 0 || largura <= 0 || altura <= 0) {
     document.getElementById('resultado').innerHTML = "Por favor, insira <b>valores válidos</b> maiores que zero.";
     return;
   }
-  document.getElementById('resultado').innerHTML = `Área total: ${metroQuadrado.toFixed(2)}</br>
-  Valor total: ${valorTotal.toFixed(2)}`;
 
-}
+  if (vernizsim) {
+    valorTotal = (metroQuadrado * 10) + valorTotal;
+  }
+
+  document.getElementById('resultado').innerHTML = `Área total: <b>${metroQuadrado.toFixed(2)} m²</b><br>Valor total: R$ <b>${valorTotal.toFixed(2)}</b>`;
+});
